b4ea96d026ceb38706ac287269c5629259a6efa1
ddnguyen93 <ddnguyen93@gmail.com>
2022-01-11 00:08:46 -0500

    Removed uri

--- server.js
+++ server.js
@@ -1,66 +1,67 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
 }
 
-var createError = require("http-errors");
-var express = require("express");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var HttpError = require("./models/http-error");
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var HttpError = require('./models/http-error');
 
 var app = express();
 
-var indexRouter = require("./routes/index");
+var indexRouter = require('./routes/index');
 
 // Import the mongoose module
-var mongoose = require("mongoose");
-var mongoDB =
-  "mongodb+srv://ddnguyenOdinbook:Chocolate1@cluster0.rrdgm.mongodb.net/odinbook_project?retryWrites=true&w=majority";
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, PUT"
-  );
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, DELETE, PUT'
+	);
+	next();
 });
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  const error = new HttpError("Page not found", 404);
-  next(error);
+	const error = new HttpError('Page not found', 404);
+	next(error);
 });
 
 // error handler
 app.use((error, req, res, next) => {
-  // // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get("env") === "development" ? err : {};
-  // // render the error page
-  // res.status(err.status || 500);
-  res.status(error.code || 500);
-  res.json({
-    error: {
-      status: error.code,
-      message: error.message,
-    },
-  });
+	// // set locals, only providing error in development
+	// res.locals.message = err.message;
+	// res.locals.error = req.app.get("env") === "development" ? err : {};
+	// // render the error page
+	// res.status(err.status || 500);
+	res.status(error.code || 500);
+	res.json({
+		error: {
+			status: error.code,
+			message: error.message,
+		},
+	});
 });
+
+// app.listen(5000, console.log('Server Running'));
 
 module.exports = app;
